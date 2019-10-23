d48383ff81bb2bd8d41c2213ad2b93112b3b571e
yansen626 <yansen626@gmail.com>
2019-10-23 10:36:04 +0700

    23 oktober - tambahan email

---
+++ resources/views/mail/user-registered.blade.php
@@ -0,0 +1,286 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+    <meta content="initial-scale=1.0" name="viewport">
+    <meta content="telephone=no" name="format-detection">
+    <title>Indofund.id - Prospectus</title>
+    <style type="text/css">
+        .socialLinks {font-size: 6px;}
+        .socialLinks a {display: inline-block;}
+        .socialIcon {display: inline-block;vertical-align: top;padding-bottom: 0px;border-radius: 100%;}
+        table.vb-row, table.vb-content {border-collapse: separate;}
+        table.vb-row {border-spacing: 9px;}
+        table.vb-row.halfpad {border-spacing: 0;padding-left: 9px;padding-right: 9px;}
+        table.vb-row.fullwidth {border-spacing: 0;padding: 0;}
+        table.vb-container.fullwidth {padding-left: 0;padding-right: 0;}
+    </style>
+    <style type="text/css">
+        /* yahoo, hotmail */
+        .ExternalClass, .ExternalClass p, .ExternalClass span, .ExternalClass font, .ExternalClass td, .ExternalClass div{ line-height: 100%; }
+        .yshortcuts a{ border-bottom: none !important; }
+        .vb-outer{ min-width: 0 !important; }
+        .RMsgBdy, .ExternalClass{
+            width: 100%;
+            background-color: #3f3f3f;
+            background-color: #3f3f3f}
+
+        /* outlook */
+        table{ mso-table-rspace: 0pt; mso-table-lspace: 0pt; }
+        #outlook a{ padding: 0; }
+        img{ outline: none; text-decoration: none; border: none; -ms-interpolation-mode: bicubic; }
+        a img{ border: none; }
+
+        @media screen and (max-device-width: 600px), screen and (max-width: 600px) {
+            table.vb-container, table.vb-row{
+                width: 95% !important;
+            }
+
+            .mobile-hide{ display: none !important; }
+            .mobile-textcenter{ text-align: center !important; }
+
+            .mobile-full{
+                float: none !important;
+                width: 100% !important;
+                max-width: none !important;
+                padding-right: 0 !important;
+                padding-left: 0 !important;
+            }
+            img.mobile-full{
+                width: 100% !important;
+                max-width: none !important;
+                height: auto !important;
+            }
+        }
+    </style>
+    <style type="text/css">
+        #ko_textBlock_4 .links-color a:visited, #ko_textBlock_4 .links-color a:hover {color: #3f3f3f;color: #3f3f3f;text-decoration: underline;}
+        #ko_footerBlock_2 .links-color a, #ko_footerBlock_2 .links-color a:link, #ko_footerBlock_2 .links-color a:visited, #ko_footerBlock_2 .links-color a:hover {color: #ccc;color: #ccc;text-decoration: underline;}
+    </style>
+</head>
+<body alink="#CCCCCC" bgcolor="#3F3F3F" style="margin: 0;padding: 0;background-color: #3f3f3f;color: #919191;" text="#919191" vlink="#CCCCCC">
+<center>
+    <!-- HEADER -->
+    <table bgcolor="#fff" border="0" cellpadding="0" cellspacing="0" class="vb-outer" id="ko_logoBlock_5" style="background-color: #fff;" width="100%">
+        <tbody>
+        <tr>
+            <td align="center" bgcolor="#fff" class="vb-outer" style="padding-left: 9px;padding-right: 9px;background-color: #fff;" valign="top">
+                <!--[if (gte mso 9)|(lte ie 8)]><table align="center" border="0" cellspacing="0" cellpadding="0" width="650"><tr><td align="center" valign="top"><![endif]-->
+                <div class="oldwebkit" style="max-width: 650px;">
+                    <table border="0" cellpadding="0" cellspacing="18" class="vb-container fullpad" style="border-collapse: separate;border-spacing: 18px;padding-left: 0;padding-right: 0;width: 100%;max-width: 650px;" width="650">
+                        <tbody>
+                        <tr>
+                            <td align="left" valign="top">
+                                <!--[if (gte mso 9)|(lte ie 8)]><table align="center" border="0" cellspacing="0" cellpadding="0" width="166"><tr><td align="center" valign="top"><![endif]-->
+                                <div class="mobile-full" style="display: inline-block; max-width: 166px; vertical-align: top; width: 100%;">
+                                    <a href="http://nama-official.com/" style="font-size: 18px; font-family: Arial, Helvetica, sans-serif; color: #f3f3f3; text-decoration: none;" target="_new"><img alt="" border="0" hspace="0" src="http://nama-official.com/images/icons/logo.jpg" style="border: 0px;display: block;width: 100%;max-width: 166px;" vspace="0" width="166"></a>
+                                </div><!--[if (gte mso 9)|(lte ie 8)]></td></tr></table><![endif]-->
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div><!--[if (gte mso 9)|(lte ie 8)]></td></tr></table><![endif]-->
+            </td>
+        </tr>
+        </tbody>
+    </table>
+    <!-- HEADER -->
+
+    <table bgcolor="#fff" border="0" cellpadding="0" cellspacing="0" class="vb-outer" id="ko_titleBlock_3" style="background-color: #fff;" width="100%">
+        <tbody>
+        <tr>
+            <td align="center" bgcolor="#fff" class="vb-outer" style="padding-left: 9px;padding-right: 9px;background-color: #fff;" valign="top">
+                <!--[if (gte mso 9)|(lte ie 8)]><table align="center" border="0" cellspacing="0" cellpadding="0" width="570"><tr><td align="center" valign="top"><![endif]-->
+                <div class="oldwebkit" style="max-width: 650px;">
+                    <table bgcolor="#FFFFFF" border="0" cellpadding="0" cellspacing="9" class="vb-container halfpad" style="border-collapse: separate;border-spacing: 9px;padding-left: 9px;padding-right: 9px;width: 100%;max-width: 650px;background-color: #fff;" width="650">
+                        <tbody>
+                        <tr>
+                            <td bgcolor="#FFFFFF" style="background-color: #ffffff; font-size: 14px; font-family: Verdana, Geneva, sans-serif; color: #3f3f3f;line-height: 1.6;">
+                                @if($order->order_status_id == 3)
+                                    <p>Thank you for your purchase!</p>
+                                    <p>
+                                        Hello {{$user->first_name}} {{$user->last_name}},<br>
+                                        Your payment has been received. We are pleased to inform you that your order has been confirmed and is underway.
+                                        Please allow 2-3 business days before dispatch, while we personalize your order.<br>
+                                        You can check the status of your order by logging into your account.
+                                        If you have any questions regarding your order, please contact us at hi@nama-official.com Monday-Friday, 9am-5pm WIB.
+                                        {{--Your order is now secured and will be processed on the 10th of June since our workshop is closed during the Lebaran Holiday.<br>--}}
+
+                                        {{--You can check the status of your order by logging into your account.--}}
+                                        {{--If you have any questions regarding your order, please contact us at hi@nama-official.com Monday-Friday.--}}
+                                    </p>
+                                @elseif($order->order_status_id == 8)
+                                    <p>Thank you for your purchase!</p>
+                                    <p>
+                                        Hello {{$user->first_name}} {{$user->last_name}},<br>
+                                        Thank you for your order!
+                                        {{--Thank you for your order!<br>--}}
+                                        {{--Your order is now secured and will be processed on the 10th of June since our workshop is closed during the Lebaran Holiday.<br>--}}
+
+                                        {{--You can check the status of your order by logging into your account.--}}
+                                        {{--If you have any questions regarding your order, please contact us at hi@nama-official.com Monday-Friday.--}}
+                                    </p>
+                                @elseif($order->order_status_id == 7)
+                                    <p>
+                                        Hello {{$user->first_name}} {{$user->last_name}},<br>
+                                        Thank you for your order!<br>
+                                        You have selected to pay by bank transfer. Please make the payment to the account below:
+                                    </p>
+                                    <p style="font-size: 20px;text-align: center">
+                                        BANK CENTRAL ASIA (BCA)<br>
+                                        006 999 1515  a.n PT. Nama Karya Bersama
+                                    </p>
+                                @endif
+                            </td>
+                        </tr>
+                        <tr>
+                            <td bgcolor="#FFFFFF" style="background-color: #ffffff; font-size: 14px; font-family: Verdana, Geneva, sans-serif; color: #3f3f3f; line-height: 1.6;">
+                               <span data-mce-style="color: #EAA2AE;" style="color:#EAA2AE;">
+                                    <strong>
+                                        <a data-mce-style="color: #EAA2AE;" href="http://nama-official.com/orders" style="color: #EAA2AE;text-decoration: underline;" title="Order History">
+                                            View Order Status
+                                        </a>
+                                    </strong>
+                                </span>
+                                <h2>ORDER DETAILS</h2>
+                                <h4>Order {{$order->order_number}}</h4>
+                                @if($order->payment_option == "Credit Card")
+                                    <h4>Payment with Credit Card</h4>
+                                @else
+                                    <h4>Payment with Bank Transfer</h4>
+                                @endif
+                                <span>
+                                    {{ \Carbon\Carbon::parse($order->created_at)->format('F j, Y')}}
+                                </span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td bgcolor="#FFFFFF" style="background-color: #ffffff; font-size: 14px; font-family: Verdana, Geneva, sans-serif; color: #3f3f3f; line-height: 1.6;">
+                                <span>
+                                    <h4>SHIPPING ADDRESS</h4>
+                                    {{$user->first_name}} {{$user->last_name}}<br>
+                                    {{$order->address->description}},
+                                    {{$order->address->street}},<br>
+                                    {{$order->address->city->name}}, {{$order->address->province->name}}, {{$order->address->postal_code}}<br>
+                                    {{$order->address->country->name}} <br>
+                                    Tel: {{$user->phone}} <br>
+
+                                </span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td bgcolor="#FFFFFF" style="background-color: #ffffff; font-size: 12px; font-family: Verdana, Geneva, sans-serif; color: #3f3f3f; line-height: 1.6;">
+
+                                <h2>ITEM DESCRIPTION</h2>
+                                <hr style="margin: 0;"/>
+                                <span>
+                                    <table bgcolor="#FFFFFF" border="0" cellpadding="0" cellspacing="9" class="vb-container halfpad" style="border-collapse: separate;border-spacing: 9px;padding-left: 9px;padding-right: 9px;width: 100%;max-width: 650px;background-color: #fff;" width="650">
+                                        <tbody>
+                                        @foreach($orderProducts as $orderProduct)
+                                            <tr>
+                                            <td bgcolor="#FFFFFF" style="background-color: #ffffff; font-size: 12px; font-family: Verdana, Geneva, sans-serif; color: #3f3f3f;line-height: 1.6;">
+                                                <img alt="" border="0" hspace="0" src="http://nama-official.com/storage/products/{{$productImages[$orderProduct->product_id]}}"
+                                                     style="border: 0px;display: block;height:100px;" vspace="0">
+                                            </td>
+                                            <td bgcolor="#FFFFFF" style="background-color: #ffffff; font-size: 12px; font-family: Verdana, Geneva, sans-serif; color: #3f3f3f;line-height: 1.6;">
+                                                <a href="http://nama-official.com/{{$orderProduct->product->slug}}">{{$orderProduct->Product->name}}</a><br>
+                                            </td>
+                                            <td bgcolor="#FFFFFF" style="background-color: #ffffff; font-size: 12px; font-family: Verdana, Geneva, sans-serif; color: #3f3f3f;line-height: 1.6;">
+                                                {!! $orderProduct->product_info !!}
+                                            </td>
+                                            <td bgcolor="#FFFFFF" style="background-color: #ffffff; font-size: 12px; font-family: Verdana, Geneva, sans-serif; color: #3f3f3f;line-height: 1.6;">
+                                                x {{$orderProduct->qty}}
+                                            </td>
+                                            <td bgcolor="#FFFFFF" style="background-color: #ffffff; font-size: 12px; font-family: Verdana, Geneva, sans-serif; color: #3f3f3f;line-height: 1.6;">
+                                                {{env('KURS_IDR')}} {{$orderProduct->grand_total_string}}
+                                            </td>
+                                        </tr>
+                                        @endforeach
+                                        </tbody>
+                                    </table>
+                                </span>
+                                <hr style="margin: 0;"/>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td bgcolor="#FFFFFF" style="background-color: #ffffff; font-size: 14px; font-family: Verdana, Geneva, sans-serif; color: #3f3f3f; line-height: 1.6;">
+
+                                <span>
+                                    <table bgcolor="#FFFFFF" border="0" cellpadding="0" cellspacing="9" class="vb-container halfpad" style="border-collapse: separate;border-spacing: 9px;padding-left: 9px;padding-right: 9px;width: 100%;max-width: 650px;background-color: #fff;" width="650">
+                                        <tbody>
+                                        <tr>
+                                            <td bgcolor="#FFFFFF" style="text-align: right;background-color: #ffffff; font-size: 12px; font-family: Verdana, Geneva, sans-serif; color: #3f3f3f;line-height: 1.6;">
+                                                Subtotal :
+                                                <br>
+                                                Shipping :
+                                                <br>
+                                                Total :
+                                            </td>
+                                            <td bgcolor="#FFFFFF" style="text-align: right;background-color: #ffffff; font-size: 12px; font-family: Verdana, Geneva, sans-serif; color: #3f3f3f;line-height: 1.6;">
+                                                {{env('KURS_IDR')}} {{$order->sub_total_string}}
+                                                <br>
+                                                {{env('KURS_IDR')}} {{$order->shipping_charge_string}}
+                                                <br>
+                                                {{env('KURS_IDR')}} {{$order->grand_total_string}}
+                                            </td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                </span>
+                                <hr style="margin: 0;"/>
+                            </td>
+                        </tr>
+                        {{--<tr>--}}
+                        {{--<td bgcolor="#FFFFFF" style="background-color: #ffffff; font-size: 14px; font-family: Verdana, Geneva, sans-serif; color: #3f3f3f; line-height: 1.6;">--}}
+
+                        {{--<h2>PAYMENT INFO</h2>--}}
+                        {{--<hr style="margin: 0;"/>--}}
+                        {{--<span>--}}
+                        {{--<span>{{$order->payment_option}}</span>--}}
+                        {{--</span>--}}
+                        {{--<hr style="margin: 0;"/>--}}
+                        {{--</td>--}}
+                        {{--</tr>--}}
+                        <tr>
+                            <td bgcolor="#FFFFFF" style="background-color: #ffffff; font-size: 10px; font-family: Verdana, Geneva, sans-serif; color: #3f3f3f;line-height: 1.6;">
+                                <p>
+                                    Terms & Conditions<br>
+                                    All of our products are custom made to order, therefore please allow 2-3 business days for production. All sales are final.<br>
+                                    <span style="font-weight: bold">>Notes: Availability of your products will only be secured after you confirm your payment.</span>
+                                </p>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <!--[if (gte mso 9)|(lte ie 8)]></td></tr></table><![endif]-->
+            </td>
+        </tr>
+        </tbody>
+    </table>
+
+    <!-- FOOTER -->
+    <hr style="margin: 0;"/>
+    <table bgcolor="#fff" border="0" cellpadding="0" cellspacing="0" class="vb-outer" id="ko_titleBlock_3" style="background-color: #fff;" width="100%">
+        <tbody>
+        <tr>
+            <td align="center" bgcolor="#fff" class="vb-outer" style="padding-left: 9px;padding-right: 9px;background-color: #fff;" valign="top">
+                <!--[if (gte mso 9)|(lte ie 8)]><table align="center" border="0" cellspacing="0" cellpadding="0" width="570"><tr><td align="center" valign="top"><![endif]-->
+                <div class="oldwebkit" style="max-width: 650px;">
+                    <table bgcolor="#FFFFFF" border="0" cellpadding="0" cellspacing="9" class="vb-container halfpad" style="border-collapse: separate;border-spacing: 9px;padding-left: 9px;padding-right: 9px;width: 100%;max-width: 650px;background-color: #fff;" width="650">
+                        <tbody>
+                        <tr>
+                            <td align="center" bgcolor="#FFFFFF" style="background-color: #ffffff; font-size: 14px; font-family: Verdana, Geneva, sans-serif; color: #3f3f3f; text-align: center; line-height: 1.6;"><span>Contact us: hi@nama-official.com</span></td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <!--[if (gte mso 9)|(lte ie 8)]></td></tr></table><![endif]-->
+            </td>
+        </tr>
+        </tbody>
+    </table>
+    <!-- FOOTER -->
+</center>
+</body>
+</html>
